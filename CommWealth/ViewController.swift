08d0dbb840d5689dd1eb36e4c20a035f0b06d6aa
Erick Kasih <erick.kasih@gmail.com>
2020-09-15 17:15:31 +0700

    Initial Commit

---
+++ CommWealth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CommWealth
+//
+//  Created by JAN FREDRICK on 15/09/20.
+//  Copyright © 2020 JFSK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
